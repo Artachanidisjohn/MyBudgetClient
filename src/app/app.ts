5d0cf5985512d2ba8a3be566e5e8a1a4c3cca6fd
Artachanidisjohn <artachanidis@hotmail.com>
2025-12-31 13:31:44 +0200

    small fixes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
@@ -8,4 +8,6 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule, RouterOutlet],
   templateUrl: './app.html',
 })
-export class App {}
+export class App {
+  protected readonly title = signal('my-budget');
+}
