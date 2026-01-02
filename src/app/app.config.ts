ee247da7b133127896c918b62e7c71d980620f84
Artachanidisjohn <artachanidis@hotmail.com>
2026-01-02 13:19:21 +0200

    fix app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,17 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
 import { provideIonicAngular } from '@ionic/angular/standalone';
 
 import { routes } from './app.routes';
+import { authInterceptor } from './components/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideIonicAngular(),
     provideRouter(routes),
-    provideHttpClient(withInterceptorsFromDi()),
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+    ),
   ],
 };
