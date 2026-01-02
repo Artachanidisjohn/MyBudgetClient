d9e0cfcdfae7d953ea3e61f21a9090091212f284
Artachanidisjohn <artachanidis@hotmail.com>
2026-01-02 12:52:53 +0200

    fix app html

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [IonApp, IonRouterOutlet],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
 })
 export class App {
